5682f3758dd8934f0fd477a74b19c796d8e3acad
gentanaka <gen.tanaka2001@gmail.com>
2021-12-23 23:31:22 +0900

    master

--- xx_RubyOnRails/school/db/seeds.rb
+++ xx_RubyOnRails/school/db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Faculty.create(name: '総合政策')
+Faculty.create(name: '環境情報')
+Course.create(name: '体育１', credit: 1, compulsory: true)
+Course.create(name: 'プログラミング言語論', credit: 2, compulsory: false)
